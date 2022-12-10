f4dcd17cb2c25decb59bfefed10ebc38fa36e84a
Michael <michael.d.lange88@gmail.com>
2022-12-09 20:58:19 -0800

    commit tree f3f9dbc058dc1cd00359f58f91f8e317ddb441ec

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(array, query){
+    return array.filter(function(name){return name.toLowerCase() === query.toLowerCase()})
+}
+
+function fuzzyMatch(array, query){
+    return array.filter(function(name){return name.startsWith(query)})
+}
+
+function matchName(array, query){
+    return array.filter(function(name){return } )
+}
+
+// we want to see if if query = name value in the object,
